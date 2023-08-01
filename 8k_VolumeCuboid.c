664f219b7e11915f94cde6804e0b1bdd374dbfab
pkozlovsky <pkozlovsky@gmail.com>
2023-08-01 10:53:18 +0200

    solution added

--- 8k_VolumeCuboid.c
+++ 8k_VolumeCuboid.c
@@ -2,3 +2,6 @@
 Bob needs a fast way to calculate the volume of a cuboid with three values: 
 the length, width and height of the cuboid. Write a function to help Bob with this calculation.
 */
+double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width * height;
+}
