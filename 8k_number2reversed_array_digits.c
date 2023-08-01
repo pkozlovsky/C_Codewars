7578d5c2aeb166d3759b705cac58835b0a5bc24c
pkozlovsky <pkozlovsky@gmail.com>
2023-08-01 10:55:12 +0200

    level 8k added

---
+++ 8k_number2reversed_array_digits.c
@@ -0,0 +1,19 @@
+/*
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+*/
+#include <stddef.h>
+#include <inttypes.h>
+
+void digitize (uint64_t n, uint8_t digits[], size_t *length_out)
+{
+  *length_out = 0;
+  do {
+    digits[(*length_out)++] = n % 10;
+    n /= 10;
+  } while(n);
+}
