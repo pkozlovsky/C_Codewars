1a3f062b9f52c0a2c704019d19bb8bc42c3aabbe
pkozlovsky <pkozlovsky@gmail.com>
2023-08-08 10:03:07 +0200

    solution added

--- 7k_IsThisAtriangle.c
+++ 7k_IsThisAtriangle.c
@@ -3,3 +3,9 @@ Implement a function that accepts 3 integer values a, b, c. The function should 
 
 (In this case, all triangles must have surface greater than 0 to be accepted).
 */
+#include <stdbool.h>
+
+bool is_triangle(int a, int b, int c)
+{
+  return a + b > c && a + c > b && b + c > a;
+}
