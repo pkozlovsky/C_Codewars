90ff560ae5ea582a69f567bb00b6fefda3b8707e
pkozlovsky <pkozlovsky@gmail.com>
2023-08-10 12:50:46 +0200

    solution addd

--- 7k_CategorizeNewMember.c
+++ 7k_CategorizeNewMember.c
@@ -14,4 +14,16 @@ input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 */
 
+#include <stddef.h>
 
+enum membership {OPEN = 1, SENIOR = 2};
+
+void open_or_senior(size_t n, const int members[n][2], enum membership memberships[n]) {
+    for (size_t i = 0; i < n; i++) {
+        if (members[i][0] >= 55 && members[i][1] > 7) {
+            memberships[i] = SENIOR;
+        } else {
+            memberships[i] = OPEN;
+        }
+    }
+}
