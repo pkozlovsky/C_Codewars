4eb1466de7eb4032e5160c9638c9df1fbc2792c4
pkozlovsky <pkozlovsky@gmail.com>
2023-08-11 09:05:27 +0200

    solved!

--- 7k_FilterTheNumber.c
+++ 7k_FilterTheNumber.c
@@ -8,4 +8,21 @@ Your task is to return a number from a string.
 Details
 You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
 */
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 
+long long filter_string(const char *value)
+{
+    long long result = 0;
+
+    while (*value) 
+    {  
+        if (isdigit(*value)) {  
+            result = result * 10 + (*value -'0');
+        }
+        value++;
+    }
+    
+    return result; 
+}
