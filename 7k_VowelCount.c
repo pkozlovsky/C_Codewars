c120639326e585f26ad172765aa8cc64b7a50f20
pkozlovsky <pkozlovsky@gmail.com>
2023-08-28 15:08:37 +0200

    solved!

--- 7k_VowelCount.c
+++ 7k_VowelCount.c
@@ -7,3 +7,34 @@ The input string will only consist of lower case letters and/or spaces.
 */
 
 
+#include <stdio.h>
+#include <stddef.h>
+
+size_t get_count(const char *s)
+{
+    int vowels[2][5] = {
+        {'a', 'e', 'i', 'o', 'u'},
+        {0, 0, 0, 0, 0}
+    };
+    
+    int len = 0;
+    const char *temp = s; 
+    while (*temp != '\0') {
+        len++;
+        temp++;
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            if (s[j] == vowels[0][i])
+            {
+                vowels[1][i]++;
+            }
+        }
+    }
+    size_t sum = 0;
+    for(int i=0; i<5;i++) sum += vowels[1][i];
+    return sum;
+}
