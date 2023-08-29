ffb2120dfd0b33346d16567b64e01a36b98e3978
pkozlovsky <pkozlovsky@gmail.com>
2023-08-29 11:48:18 +0200

    initial commit

---
+++ 6k_polygonArea.c
@@ -0,0 +1,16 @@
+/*
+Write the following function:
+
+double area_of_polygon_inside_circle(double circle_radius, int number_of_sides);
+It should calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle). The answer should be a number rounded to 3 decimal places.
+
+Input :: Output Examples
+
+area_of_polygon_inside_circle(3, 3); // => 11.691
+
+area_of_polygon_inside_circle(5.8, 7); // => 92.053
+
+area_of_polygon_inside_circle(4, 5); // => 38.042
+*/
+
+
