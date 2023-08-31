5df07855811d47fa874802d1a821fb177cbdbed0
pkozlovsky <pkozlovsky@gmail.com>
2023-08-31 13:29:10 +0200

    solved!

--- 6k_WhoLikesIt.c
+++ 6k_WhoLikesIt.c
@@ -11,4 +11,28 @@ Implement the function which takes an array containing the names of people that 
 return must be an allocated string
 do not mutate input
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char *likes(size_t n, const char *const names[n]) {
+    char *result = NULL;
+    switch (n) {
+        case 0:
+            result = strdup("no one likes this");
+            break;
+        case 1:
+            asprintf(&result, "%s likes this", names[0]);
+            break;
+        case 2:
+            asprintf(&result, "%s and %s like this", names[0], names[1]);
+            break;
+        case 3:
+            asprintf(&result, "%s, %s and %s like this", names[0], names[1], names[2]);
+            break;
+        default:
+            asprintf(&result, "%s, %s and %zu others like this", names[0], names[1], n - 2);
+            break;
+    }
+    return result;
+}
