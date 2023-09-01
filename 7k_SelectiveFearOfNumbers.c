5abc84f760540b043318e3375cd2829d534501f7
pkozlovsky <pkozlovsky@gmail.com>
2023-09-01 15:30:12 +0200

    solved!

--- 7k_SelectiveFearOfNumbers.c
+++ 7k_SelectiveFearOfNumbers.c
@@ -18,4 +18,18 @@ Sunday --> 666 or -666
 Write a function which takes a string (day of the week) and an integer (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
 */
 
+#include <stdbool.h>
 
+
+bool am_i_afraid(const char *day, int num)
+{
+    return (
+        (strcmp(day, "Monday") == 0 && num == 12) ||
+        (strcmp(day, "Tuesday") == 0 && num > 95) ||
+        (strcmp(day, "Wednesday") == 0 && num == 34) ||
+        (strcmp(day, "Thursday") == 0 && num == 0) ||
+        (strcmp(day, "Friday") == 0 && num % 2 == 0) ||
+        (strcmp(day, "Saturday") == 0 && num == 56) ||
+        (strcmp(day, "Sunday") == 0 && (num == 666 || num == -666))
+    );
+}
