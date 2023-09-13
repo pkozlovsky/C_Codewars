37e7119ea8d3296d88018d010f1f36fe5e6ba5a7
pkozlovsky <pkozlovsky@gmail.com>
2023-09-13 12:59:40 +0200

    solved!

--- 7k_OnesAndZeros.c
+++ 7k_OnesAndZeros.c
@@ -14,5 +14,17 @@ Testing: [0, 1, 1, 0] ==> 6
 Testing: [1, 1, 1, 1] ==> 15
 Testing: [1, 0, 1, 1] ==> 11
 */
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
 
+unsigned binary_array_to_numbers(const unsigned *bits, size_t count){
+   
+    int result = 0;
+    
+    for(int i = count - 1; i >= 0; i--) {
+        if(bits[i] == 1) result += pow(2, count - 1 - i);
+    }
+    return result;
+}
 
