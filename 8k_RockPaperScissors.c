7948d2be05f66830435eba0081eea8109ad87419
pkozlovsky <pkozlovsky@gmail.com>
2023-09-20 13:08:12 +0200

    solved

--- 8k_RockPaperScissors.c
+++ 8k_RockPaperScissors.c
@@ -8,4 +8,19 @@ Examples(Input1, Input2 --> Output):
 "scissors", "rock" --> "Player 2 won!"
 "paper", "paper" --> "Draw!"
 */
+enum tool {ROCK, PAPER, SCISSORS};
+enum outcome {P1_WON, P2_WON, DRAW};
 
+enum outcome rps (enum tool p1, enum tool p2)
+{
+    if(p1 == p2) return DRAW;
+
+    switch(p1) {
+        case ROCK:
+            return (p2 == SCISSORS) ? P1_WON : P2_WON;
+        case PAPER:
+            return (p2 == ROCK) ? P1_WON : P2_WON;
+        case SCISSORS:
+            return (p2 == PAPER) ? P1_WON : P2_WON;
+    }
+}
